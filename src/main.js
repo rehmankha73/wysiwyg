95200efcf0e91b173fe67b12fd06cc1cb5042375
Rehman Ahmed Khan <72739116+rehmankha73@users.noreply.github.com>
2022-06-29 09:10:37 +0500

    implementation of apexchart with vue (WIP).

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import vuetify from './plugins/vuetify'
 import store from './store'
 import Vue2Editor from "vue2-editor";
 import './assets/styles.css'
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
 Vue.use(Vue2Editor);
+Vue.use(VueApexCharts)
 Vue.component('quill-component', require('./components/QuillCompenent').default);
+// eslint-disable-next-line vue/multi-word-component-names
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Chart from '@/views/LineChart'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/BlogView.vue')
-  }
+  },
+  {
+    path: '/chart',
+    name: 'Chart',
+    component: Chart
+  },
 ]
 
 const router = new VueRouter({
